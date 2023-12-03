1b40a4f251be08d1e911343d500667400df0dfb8
hejiehao <hejiehao12@126.com>
2023-12-03 23:18:43 +0800

    progress

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("push test");
+        for (int i = 0; i <= 100; i++) {
+            System.out.print("\r" + "Progress: " + i + "%");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
