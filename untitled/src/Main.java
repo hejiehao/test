75472d4b27623db986147fe10e7e9ca54d5a3753
hejiehao <hejiehao12@126.com>
2024-02-25 15:31:51 +0800

    Update .github/ISSUE_TEMPLATE/bug2.yml Co-authored-by: 柚子柚子 <youzi5201086@163.com>

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i <= 100; i++) {
-            System.out.print("\r" + "Progress: " + i + "%");
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        System.out.println("test");
     }
 }
